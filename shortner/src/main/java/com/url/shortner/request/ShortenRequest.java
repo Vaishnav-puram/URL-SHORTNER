669f931e69091e5da7d0848c067bdf893652a5f2
Vaishnav-puram <puramv307@gmail.com>
2025-01-25 15:01:07 +0530

    dto added

---
+++ shortner/src/main/java/com/url/shortner/request/ShortenRequest.java
@@ -0,0 +1,12 @@
+package com.url.shortner.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ShortenRequest {
+    private String url;
+}
